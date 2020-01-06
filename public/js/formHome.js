9b0d55383e8c9fa628623a5798d09b438f9224a5
Chicão Thiago <fthiagogv@gmail.com>
2020-01-06 01:22:56 -0300

    last files

---
+++ public/js/formHome.js
@@ -0,0 +1,148 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/formHome.js":
+/*!**********************************!*\
+  !*** ./resources/js/formHome.js ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function changeFormToRegister() {
+  $('#form-register-pass-confirm').removeAttr('hidden');
+  $('#form-register-name').removeAttr('hidden');
+  $('#form-register-name').attr('required', true);
+  $('#form-buttom-register').text('Já sou cadastrado!');
+  $('#form-button-login').text('Registrar');
+}
+
+function changeFormToLogin() {
+  $('#form-register-pass-confirm').attr('hidden', true);
+  $('#form-register-name').attr('hidden', true);
+  $('#form-register-name').removeAttr('required');
+  $('#form-buttom-register').text('Primeira vez? Cadastre-se!');
+  $('#form-button-login').text('Entrar');
+}
+
+$('#form-buttom-register').on('click', function () {
+  if ($('#form-button-login').text().trim().toString() === 'Entrar') {
+    return changeFormToRegister();
+  } else {
+    return changeFormToLogin();
+  }
+});
+$('#form-button-login').on('click', function () {
+  if ($('#form-button-login').text().trim().toString() === 'Entrar') {
+    $('#form-home').attr('action', '/login');
+    $('#form-home').submit();
+  } else {
+    $('#form-home').attr('action', '/register');
+    $('#form-home').submit();
+  }
+});
+$('#form-home input').keypress(function (e) {
+  if (e.which == 13) {
+    $('#form-home').submit();
+  }
+});
+
+/***/ }),
+
+/***/ 1:
+/*!****************************************!*\
+  !*** multi ./resources/js/formHome.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/chicao/Projetos/funceme_mettings/resources/js/formHome.js */"./resources/js/formHome.js");
+
+
+/***/ })
+
+/******/ });
